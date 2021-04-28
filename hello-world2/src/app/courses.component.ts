c0dcb599dd035b8cee01995e5584dfb9df18becb
Balazs Rigo <rigo.balazs86@gmail.com>
2021-04-28 22:59:02 +0200

    8- Event Filtering

--- hello-world2/src/app/courses.component.ts
+++ hello-world2/src/app/courses.component.ts
@@ -3,20 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
   template: `
-    <div (click)="onDivClicked()">
-    <button (click)="onSave($event)">Save<button>
-    <div>
-`
+    <input (keyup.enter)="onKeyUp()" />
+  `
 })
 
 export class CoursesComponent {
-  onDivClicked() {
-    console.log("Div was clicked!");
-  }
-
-
-  onSave($event: any) {
-    $event.stopPropagation();
-    console.log("Button was clicked!", $event);
+  onKeyUp() {
+    console.log("Enter was pressed");
   }
 }
