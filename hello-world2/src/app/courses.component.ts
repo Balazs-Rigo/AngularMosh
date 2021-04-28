3bd76993d16dba9248196fe4d98f2359efca5352
Balazs Rigo <rigo.balazs86@gmail.com>
2021-04-28 20:37:15 +0200

    Class binding es egyeb kiegeszitesek a 2es projektben. mostmar mukodik.

--- hello-world2/src/app/courses.component.ts
+++ hello-world2/src/app/courses.component.ts
@@ -3,12 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
   template: `
-    <button class="btn btn-primary">Save<button>
+    <button class="btn btn-primary" [class.active]="isActive">Save<button>
     `
 })
 
 export class CoursesComponent {
-  title = "List of courses";
-  imageUrl = "http://lorempixel.com/400/200";
-
+  isActive = false;
 }
