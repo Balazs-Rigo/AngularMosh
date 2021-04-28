c7ce513577f001f23407c9ac3728fc9e2729484e
Balazs Rigo <rigo.balazs86@gmail.com>
2021-04-28 22:55:59 +0200

    7- Event Binding

--- hello-world2/src/app/courses.component.ts
+++ hello-world2/src/app/courses.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
   template: `
-    <button [style.backgroundcolor]="isActive ? 'blue' : 'white' ">Save<button>
-    `
+    <div (click)="onDivClicked()">
+    <button (click)="onSave($event)">Save<button>
+    <div>
+`
 })
 
 export class CoursesComponent {
-  isActive = true;
+  onDivClicked() {
+    console.log("Div was clicked!");
+  }
+
+
+  onSave($event: any) {
+    $event.stopPropagation();
+    console.log("Button was clicked!", $event);
+  }
 }
