9bd58ae6e47d1b4b82268e511ed5d43cd20361b2
Balazs Rigo <rigo.balazs86@gmail.com>
2021-04-29 18:48:16 +0200

    favorite exercise

--- hello-world2/src/app/app.module.ts
+++ hello-world2/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
